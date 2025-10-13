e0368091c04610e323180230fdd6b020aa9ad71f
rishanlpw <rishan@lpw.lk>
2025-10-13 11:44:26 +0530

    new

---
+++ .scripts/deploy.sh
@@ -0,0 +1,20 @@
+set -e
+
+echo "Starting deployment..."
+
+(php artisan down) || true
+
+git pull origin main
+
+composer install --optimize-autoloader --no-dev --no-interaction
+
+php artisan cache:clear
+php artisan config:clear
+
+php artisan optimize
+
+php artisan migrate --force
+
+php artisan up
+
+echo "Deployment completed successfully."
